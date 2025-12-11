9d3cb9adbeaea747a8114be475cd00a75d5aeb3b
znarf0110 <it_franz@creazionstudios.com>
2025-12-11 11:09:46 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyBv0Z-xF1-M3Hfe078dT6tpOj096IusQWU",
     authDomain: "accounts-database-5b52b.firebaseapp.com",
     projectId: "accounts-database-5b52b",
-    storageBucket: "accounts-database-5b52b.firebasestorage.app",
+    storageBucket: "accounts-database-5b52b.appspot.com",
     messagingSenderId: "889753057532",
     appId: "1:889753057532:web:531b7a357dc4e9aa93bf79"
 };
@@ -16,4 +16,5 @@ firebase.initializeApp(firebaseConfig);
 // Define global service objects accessible by your login/dashboard functions
 const auth = firebase.auth();
 const db = firebase.firestore();
+
 
